276340fc0beb297cecd1bbadb15ab1c225ccad95
Dagmawi-Y <dagmawi.inbox@gmail.com>
2023-06-15 11:46:49 +0300

    http response fix

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:clima_flutter/services/location.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
+
+const apiKey = 'ac50e642c12519e8ff3ca1eca5cf431a';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -15,6 +17,18 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print(location.longitude);
   }
 
+  void getData() async {
+    String url =
+        'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=$apiKey';
+    Uri uri = Uri.parse(url);
+    http.Response response = (await http.get(uri)) as http.Response;
+    if (response.statusCode == 200) {
+      String data = response.body;
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   @override
   void initState() {
     super.initState();
@@ -23,6 +37,11 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
+    try {
+      getData();
+    } catch (e) {
+      print(e);
+    }
     return Scaffold();
   }
 }
