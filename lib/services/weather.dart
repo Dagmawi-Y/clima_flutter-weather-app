646c6e2ac5040e025d17e02627bde36ee4898222
Dagmawi-Y <dagmawi.inbox@gmail.com>
2023-06-16 15:57:03 +0300

    passing data backwards through navigation stack from the cityscreen

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -5,6 +5,13 @@ const apiKey = 'ac50e642c12519e8ff3ca1eca5cf431a';
 const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
 
 class WeatherModel {
+  Future<dynamic> getCityWeather(String cityName) async {
+    NetworkHelper networkHelper = NetworkHelper(
+        '$openWeatherMapURL?q=$cityName&appid=$apiKey&units=metric');
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
   Future<dynamic> getLocationWeather() async {
     Location location = Location();
     await location.getCurrentLocation();
